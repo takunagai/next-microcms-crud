dfe355bc4d77512d7adb6af17f8152752ca2762b
takna <tak_na@icloud.com>
2024-04-24 13:23:02 +0900

    Todo作成機能を追加
    ページコンポーネントにTodo作成機能を追加しました。ユーザーは新規Todoをフォームに入力し、それがリストに表示されるようになります。また、その変更は`revalidatePath`関数によって反映されます。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { client } from "@/lib/client";
+import { revalidatePath } from "next/cache";
 
 type dataType = {
 	contents: contentsType[];
@@ -12,6 +13,19 @@ type contentsType = {
 	title: string;
 };
 
+const createTodo = async (formData: FormData) => {
+	"use server";
+	const title = formData.get("title") as string;
+	const sendData = `{"title":"${title}"}`;
+
+	await client.create({
+		endpoint: "todo",
+		content: JSON.parse(sendData),
+	});
+
+	revalidatePath("/", "page");
+};
+
 export default async function Home() {
 	const data: dataType = await client.get({
 		endpoint: "todo",
@@ -21,17 +35,33 @@ export default async function Home() {
 	return (
 		<div className="flex flex-col gap-4">
 			<h1 className="text-2xl">Todo List</h1>
-			{data?.totalCount ? (
-				<ul>
-					{data?.contents.map((value) => (
-						<li key={value.id} className="list-inside list-disc">
-							{value.title}
-						</li>
-					))}
-				</ul>
-			) : (
-				<p>No data</p>
-			)}
+			<div>
+				<form className="flex gap-2" action={createTodo}>
+					<input
+						type="text"
+						name="title"
+						placeholder="Todo を入力…"
+						defaultValue={""}
+						className="text-gray-800 px-1"
+					/>
+					<button type="submit" className="px-2 bg-gray-700 hover:bg-gray-600">
+						入力する
+					</button>
+				</form>
+			</div>
+			<div>
+				{data?.totalCount ? (
+					<ul>
+						{data?.contents.map((value) => (
+							<li key={value.id} className="list-inside list-disc">
+								{value.title}
+							</li>
+						))}
+					</ul>
+				) : (
+					<p>No data</p>
+				)}
+			</div>
 		</div>
 	);
 }
